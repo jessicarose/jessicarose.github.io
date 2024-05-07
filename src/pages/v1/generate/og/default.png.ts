000b3850b3baa280135cbd5e2ee8aac2b4ad9a73
Jessica Rose <jessicarose@Jessicas-MacBook-Air-2.local>
2024-05-07 19:35:44 +0100

    fixed preview

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -19,13 +19,13 @@ export const GET: APIRoute = async () => {
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">Brutal</p>
-          <p style="font-size: 38px;">A theme for Astro</p>
-          <p style="font-size: 38px;">Brought to you by Elian</p>
+          <p style="font-size: 48px;">Jess's Website</p>
+          <p style="font-size: 38px;">Mostly useful if you need to know something about Jess Rose or want that link to the pen museum.</p>
+          <p style="font-size: 38px;">👋</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <img src="https://www.jessica.tech/src/assets/happy-jess.png" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
